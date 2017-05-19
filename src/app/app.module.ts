64a5a825d8ac41b3c9e8e99b1b94818555517745
msiddiqi <muhammadsiddiqi@Muhammads-MacBook-Pro.local>
2017-05-19 08:28:23 -0400

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
+ 
 import { AppComponent } from './app.component';
-
+import { ComponentOneComponent } from './component-one/component-one.component';
+import { ComponentTwoComponent } from './component-two/component-two.component';
+ 
+const appRoutes : Routes = [
+  { path: "componentOne", component: ComponentOneComponent},
+  { path: "componentTwo", component: ComponentTwoComponent}
+]
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentOneComponent,
+    ComponentTwoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
